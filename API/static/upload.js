a7273948f575ddbfe1e76f1ee047abefbfcf6ffa
Shiny Andy <nqan1610@gmail.com>
2022-11-08 11:16:25 +0700

    lol

--- API/static/upload.js
+++ API/static/upload.js
@@ -12,13 +12,5 @@ function readURL(input) {
         reader.readAsDataURL(input.files[0]);
     }
     }
-function deleteOld(){
-    const fs = require('fs')
-    const path = 'C:/Users/nqan1/OneDrive/Documents/Coding_2022/Coding_Project/XuLyAnh/FireDetectionWeb/API/uploaded/test_image/non_fire.36.png'
-    try {
-  fs.unlinkSync(path)
-  //file removed
-} catch(err) {
-  console.error(err)
-}
-}
+
+
